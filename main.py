758ff55d709b3ed9207c1fff64ee2363f06bec0c
Sibren <sibrenvansetten@hotmail.nl>
2022-12-21 14:32:53 +0100

    Changed script run time

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from osm_requests import fetch_benches
 
 print("Starting script...")
 
-schedule.every().day.at("00:00").do(fetch_benches.store_osm_benches)
+schedule.every().day.at("06:00").do(fetch_benches.store_osm_benches)
 
 # Keep the script running
 while True:

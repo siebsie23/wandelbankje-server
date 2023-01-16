62025c241a4bd4fe521ae36140b4a51670e1b78c
Sibren <sibrenvansetten@hotmail.nl>
2023-01-16 20:27:46 +0100

    Added created_at to the script

--- config.py
+++ config.py
@@ -3,3 +3,11 @@ mysql_port = 3306
 mysql_user = 'root'
 mysql_pass = 'wandelBankjePass@123'
 mysql_database = 'wandelbankje_db'
+
+
+# # Local
+# mysql_host = 'localhost'
+# mysql_port = 3306
+# mysql_user = 'root'
+# mysql_pass = ''
+# mysql_database = 'wandelbankje-admin'
